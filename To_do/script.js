8d0b4fa6ea387c64d7b0d6da43e97e3ef7920e46
Him_garg <himanshugarg738@gmail.com>
2021-05-20 11:04:19 +0530

    Implementing local storage

--- To_do/script.js
+++ To_do/script.js
@@ -2,6 +2,26 @@
 
 let inputBox=document.querySelector(".input-box");
 let ul=document.querySelector(".task-list");
+let arr=[];
+
+if(localStorage.getItem("mytasks")){
+    let strarr=localStorage.getItem("mytasks");
+    arr=JSON.parse(strarr);
+    for(let i=0;i<arr.length;i++){
+        let li=document.createElement("li");
+        ul.appendChild(li);
+        li.textContent=arr[i];
+        li.setAttribute("class","task");
+        li.addEventListener("dblclick",function(){
+            li.remove();
+            // remove local storage
+            let idx=arr.indexOf(li.innerText);
+            arr.splice(idx,1);
+            strarr=JSON.stringify(arr);
+            localStorage.setItem("mytasks",strarr);
+        });
+    }
+}
 inputBox.addEventListener("keydown",function(object){
     if(object.key=="Enter" && inputBox.value!=""){
         let value=inputBox.value;
@@ -9,6 +29,9 @@ inputBox.addEventListener("keydown",function(object){
         let li=document.createElement("li");
         ul.appendChild(li);
         li.textContent=value;
+        arr.push(value);
+        let strarr=JSON.stringify(arr);
+        localStorage.setItem("mytasks",strarr);
         li.setAttribute("class","task");
         li.addEventListener("dblclick",function(){
             li.remove();
